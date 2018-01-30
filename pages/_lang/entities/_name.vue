ebe049a4d2fe327a2985267fdb2d1a113f77c7e6
Barry Fisher <barry.fisher@reallifedigital.com>
2018-01-30 02:23:06 +0000

    Entity field display improvements and error handling

--- pages/_lang/entities/_name.vue
+++ pages/_lang/entities/_name.vue
@@ -5,7 +5,12 @@
         <EntityList />
       </v-flex>
       <v-flex xs12 lg10>
-        <v-card dark color="secondary">
+        <v-card dark color="secondary" v-if="graphqlError">
+          <v-alert color="error" icon="warning" value="true">
+            There was an error loading {{ entity }}
+          </v-alert>
+        </v-card>
+        <v-card dark color="secondary" v-if="!graphqlError">
           <v-card-title>
             <div>
               <h1 class="headline mb-0">{{ entity }}</h1>
@@ -13,48 +18,50 @@
             </div>
           </v-card-title>
           <v-card-text>
-
             <v-breadcrumbs>
-              <v-breadcrumbs-item v-for="(fieldPath, index) in fieldPaths" :key="fieldPath.id" @click.native="setFieldPathActive(fieldPath.id)" :disabled="index +1 === fieldPaths.length">
+              <v-breadcrumbs-item v-for="(fieldPath, index) in fieldPaths" :key="fieldPath.path" @click.native="setFieldPathActive(fieldPath.path)" :disabled="index +1 === fieldPaths.length">
                 {{ fieldPath.label }}
               </v-breadcrumbs-item>
               <v-icon slot="divider">forward</v-icon>
             </v-breadcrumbs>
 
-            <v-data-table v-bind:headers="fieldHeaders" :items="fields" hide-actions class="elevation-1">
-              <template slot="items" slot-scope="props">
-                <td>
-                  {{ props.item.label }}
-                  <v-tooltip v-model="show" top v-if="!!props.item.description">
-                    <v-btn icon slot="activator">
-                      <v-icon color="grey lighten-1">info</v-icon>
-                    </v-btn>
-                    <span>{{ props.item.description }}</span>
-                  </v-tooltip>
-                </td>
-                <td>
-                  <code v-html="getFieldPropertyPath(props.item)"></code>
-                </td>
-                <td>
-                  <span class="field-type">
-                    {{ props.item.type }}
-                    <span class="field-type--cardinality ml-2">{{ !!props.item.many ? 'Many' : 'One' }}</span>
-                  </span>
-                  <span class="field-type--reference" v-if="props.item.type === 'reference'">
-                    <v-icon color="grey lighten-1" class="ml-4">keyboard_arrow_right</v-icon>
-                    <v-btn small flat dark color="primary" @click="redirectToEntity(props.item.references)">{{ props.item.references }}</v-btn>
-                  </span>
-                  <span class="field-type--reference" v-if="props.item.type === 'object'">
-                    <v-icon color="grey lighten-1" class="ml-4">keyboard_arrow_right</v-icon>
-                    <v-btn small flat dark color="primary" @click="gotoField(props.item._meta.camel, props.item.label)">{{ props.item.label }}</v-btn>
-                  </span>
-                </td>
-                <td>
-                  <v-icon color="grey lighten-1" v-if="!!props.item.required">check</v-icon>
-                  <v-icon color="grey lighten-1" v-if="!props.item.required">clear</v-icon>
-                </td>
-              </template>
-            </v-data-table>
+            <div class="fields-table-container">
+              <v-data-table :class="fieldsTableClasses" :headers="fieldHeaders" :items="fields" hide-actions class="elevation-1">
+                <template slot="items" slot-scope="props">
+                  <td class="field-label">
+                    {{ props.item.label }}
+                    <v-tooltip v-model="show" top v-if="!!props.item.description">
+                      <v-btn icon slot="activator">
+                        <v-icon color="grey lighten-1">info</v-icon>
+                      </v-btn>
+                      <span>{{ props.item.description }}</span>
+                    </v-tooltip>
+                  </td>
+                  <td>
+                    <code v-html="getFieldPropertyPath(props.item)"></code>
+                  </td>
+                  <td class="field-type">
+                    <span>
+                      {{ props.item.type }}
+                      <span class="field-type--cardinality">{{ !!props.item.many ? 'Many' : 'One' }}</span>
+                    </span>
+                    <span v-if="props.item.type === 'reference'">
+                      <v-icon color="grey lighten-1">keyboard_arrow_right</v-icon>
+                      <v-btn small flat dark color="primary" @click="redirectToEntity(props.item.references)">{{ props.item.references }}</v-btn>
+                    </span>
+                    <span v-if="props.item.type === 'object'">
+                      <v-icon color="grey lighten-1">keyboard_arrow_right</v-icon>
+                      <v-btn small flat dark color="primary" @click="gotoField(`${fieldPathActive}.${props.item._meta.camel}`, props.item.label)">{{ props.item.label }}</v-btn>
+                    </span>
+                  </td>
+                  <td>
+                    <v-icon color="grey lighten-1" v-if="!!props.item.required">check</v-icon>
+                    <v-icon color="grey lighten-1" v-if="!props.item.required">clear</v-icon>
+                  </td>
+                </template>
+              </v-data-table>
+            </div>
+
           </v-card-text>
         </v-card>
       </v-flex>
@@ -84,22 +91,48 @@ export default {
       })
       return false
     },
-    setFieldPathActive: function (path) {
-      if (path) {
-        this.fieldPathActive = path
-        this.fieldPaths = this.fieldPaths.filter(fp => _(path).startsWith(fp.id))
+    ensureFieldsContainerHeight: function () {
+      if (document) {
+        const fieldsTable = document.getElementsByClassName('fields-table')
+        const fieldsTableContainer = document.getElementsByClassName('fields-table-container')
+        fieldsTableContainer[0].style.height = fieldsTable[0].clientHeight + 'px'
       }
     },
-    appendAndSetFieldPathActive: function (id, label) {
-      const appendedFieldPath = `${this.fieldPathActive}.${id}`
-      this.setFieldPathActive(appendedFieldPath)
+    setFieldPathActive: function (path) {
+
+      // Ignore changing path if it's already active.
+      if (path === this.fieldPathActive) {
+        return
+      }
+
+      this.ensureFieldsContainerHeight()
+
+      // Determine the slide action direction.
+      const deeperPath = path.length > this.fieldPathActive.length
+      const initialTransition = deeperPath ? 'left' : 'right'
+      const resolveTransition = deeperPath ? 'right' : 'left'
+
+      this.fieldsTableTransition = initialTransition
+
+      // Initial slide out and update table.
+      setTimeout(() => {
+        this.fieldPathActive = path
+        this.fieldPaths = this.fieldPaths.filter(fp => _(path).startsWith(fp.path))
+        this.fieldsTableTransition = resolveTransition
+      }, 400)
+
+      // Slide back into view.
+      setTimeout(() => {
+        this.fieldsTableTransition = null
+        this.ensureFieldsContainerHeight()
+      }, 800)
+    },
+    gotoField: function (path, label) {
       this.fieldPaths.push({
-        path: appendedFieldPath,
+        path: path,
         label: label
       })
-    },
-    gotoField: function (id, label) {
-      this.appendAndSetFieldPathActive(id, label)
+      this.setFieldPathActive(path)
     },
     getFieldPropertyPath: function (field) {
       return [
@@ -111,11 +144,6 @@ export default {
         field._meta.camel
       ].filter(i => !!i).join('.')
     }
-  },
-  validate({ params }) {
-    // @todo Must be a valid entity
-    // return /^Cat+$/.test(params.name)
-    return true
   },
   computed: {
     fields: function () {
@@ -141,6 +169,14 @@ export default {
         return ''
       }
       return JSON.parse(this.entityData).description
+    },
+    fieldsTableClasses: function () {
+      const classes = ['fields-table']
+      if (this.fieldsTableTransition) {
+        classes.push('transitioning')
+        classes.push(this.fieldsTableTransition)
+      }
+      return classes
     }
   },
   data() {
@@ -153,7 +189,7 @@ export default {
           value: 'label'
         },
         {
-          text: 'Property path',
+          text: 'Field path',
           value: 'id'
         },
         {
@@ -171,11 +207,13 @@ export default {
       }),
       fieldPaths: [
         {
-          id: 'all',
+          path: 'all',
           label: 'All fields',
         }
       ],
-      fieldPathActive: 'all'
+      fieldPathActive: 'all',
+      fieldsTableTransition: null,
+      graphqlError: false,
     }
   },
   apollo: {
@@ -188,7 +226,15 @@ export default {
           }
         },
         fetchPolicy: 'cache-and-network',
-        update: data => data.ENTITY.data
+        update: (data) => {
+          if (data.ENTITY) {
+            return data.ENTITY.data
+          }
+          this.graphqlError = true
+        },
+        error(error) {
+          graphqlError = error
+        },
       }
     },
   },
@@ -200,15 +246,41 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.field-label {
+  white-space: nowrap;
+}
 .field-type {
   text-transform: uppercase;
   font-size: 0.9em;
+  white-space: nowrap;
 }
 .field-type--cardinality {
   color: $color-primary;
   font-weight: bold;
   vertical-align: super;
   font-size: 0.8em;
+}
+
+.fields-table-container {
+  transition: all 0.3s ease;
+}
+.fields-table {
+  transition: all 0.4s ease;
+  opacity: 1;
+  overflow-x: hidden;
+  .datatable {
+    transition: all 0.4s ease;
+    transform: translateX(0);
+  }
+  &.transitioning {
+    opacity: 0;
+    &.left .datatable {
+      transform: translateX(-50%);
+    }
+    &.right .datatable {
+      transform: translateX(50%);
+    }
+  }
 }
 </style>
