1aea1de3a37f8fe078423e5769bd6c00f3f3db84
Barry Fisher <barry.fisher@reallifedigital.com>
2018-02-12 02:01:36 +0000

    Improves layout, validation and display rules for FieldEdit component.

--- components/FieldEdit.vue
+++ components/FieldEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="opened" persistent max-width="75%">
+  <v-dialog v-model="opened" persistent>
 
     <v-btn fab dark small :color="iconBackgroundColor" slot="activator">
       <v-icon color="grey darken-4">{{ this.icon }}</v-icon>
@@ -12,29 +12,53 @@
       </v-card-title>
       <v-card-text>
         <v-form v-model="valid" ref="fieldEditForm" lazy-validation>
-          <small>*{{ $t('entities.fields.edit.indicatesRequiredField')}}</small>
-          <v-container grid-list-md>
+          <v-container fluid grid-list-xl>
+            <p>*{{ $t('entities.fields.edit.indicatesRequiredField')}}</p>
             <v-layout wrap>
-              <v-flex xs12 sm6>
-                <v-text-field v-model="fieldFormData.label" label="Label" :rules="rules.required" required @keyup="autoPopulateMachineName()"></v-text-field>
+              <v-flex xs12 lg6>
+                <v-text-field box v-model="fieldFormData.label" label="Label" v-if="showFormElement('label')" :disabled="disableFormElement('label')" :rules="rules.required" required @keyup="deriveMachineNameFromLabel()"></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6>
-                <v-text-field :label="$t('entities.fields.attributes.machineName')" v-model="fieldFormData.machineName" @keyup="machineNameManuallyAltered = true" hint="The addressable key for the field. Will be converted to camelCase" :rules="rules.required" required></v-text-field>
+              <v-flex xs12 lg6>
+
+                <v-text-field box :label="$t('entities.fields.attributes.machineName')" v-if="showFormElement('machineName')" :disabled="disableFormElement('machineName')" v-model="fieldFormData.machineName" @keyup="machineNameChanged()" hint="The addressable key for the field. Will be converted to camelCase" :rules="rules.required" required spellcheck="false"></v-text-field>
+
+                <div class="text-lg-center pa-2">
+                  <div v-if="machineNameAlterable">
+                    <v-icon color="green">brightness_auto</v-icon>
+                    <span v-if="showFormElement('machineNameAutoText')" class="pl-2">Machine name auto generated from label.</span>
+                  </div>
+                  <div v-if="!machineNameAlterable">
+                    <v-icon color="red">lock</v-icon>
+                    <span v-if="showFormElement('machineNameLockText')" class="pl-2">Machine name locked from label changes. Delete it to re-sync.</span>
+                    <span v-if="showFormElement('machineNameLockIdText')" class="pl-2">Machine name cannot be changed for
+                      <code>id</code> field.</span>
+                  </div>
+                </div>
+
+              </v-flex>
+
+            </v-layout>
+
+            <v-text-field box :label="$t('entities.fields.attributes.description')" v-if="showFormElement('description')" :disabled="disableFormElement('description')" v-model="fieldFormData.description" hint="A short description about the context of this field"></v-text-field>
+
+            <v-layout wrap>
+              <v-flex xs12 lg6>
+                <v-select :label="$t('entities.fields.attributes.type')" v-if="showFormElement('type')" :disabled="disableFormElement('type')" :items="fieldTypes" v-model="fieldFormData.type" :rules="rules.required" required></v-select>
+                <v-select :label="$t('entities.fields.attributes.references')" v-if="showFormElement('references')" :disabled="disableFormElement('references')" :items="entityTypes" v-model="fieldFormData.references" :rules="rules.references"></v-select>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <v-switch :label="$t('entities.fields.attributes.required')" v-if="showFormElement('required')" :disabled="disableFormElement('required')" v-model="fieldFormData.required" color="success" hide-details></v-switch>
+                <v-switch :label="$t('entities.fields.cardinality.many')" v-if="showFormElement('many')" :disabled="disableFormElement('many')" v-model="fieldFormData.many" color="success" hide-details></v-switch>
               </v-flex>
             </v-layout>
-            <v-text-field :label="$t('entities.fields.attributes.description')" v-model="fieldFormData.description" textarea hint="A short description about the context of this field"></v-text-field>
-
-            <v-select :label="$t('entities.fields.attributes.type')" :items="fieldTypes" v-model="fieldFormData.type" :rules="rules.required" required></v-select>
-            <v-select :label="$t('entities.fields.attributes.references')" v-if="fieldFormData.type === 'reference'" :items="entityTypes" v-model="fieldFormData.references" :rules="rules.references"></v-select>
-            <v-switch :label="$t('entities.fields.attributes.required')" v-model="fieldFormData.required" color="success" hide-details></v-switch>
-            <v-switch :label="$t('entities.fields.cardinality.many')" v-model="fieldFormData.many" color="success" hide-details></v-switch>
 
           </v-container>
         </v-form>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="green darken-1" flat @click.native="save()">{{ $t('entities.fields.edit.save') }}</v-btn>
+        <v-btn color="green darken-1" flat @click="submit">{{ $t('entities.fields.edit.save') }}</v-btn>
+        <v-btn color="grey darken-1" flat @click="cancel">{{ $t('entities.fields.edit.cancel') }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -46,31 +70,36 @@
 
   export default {
     data() {
+
+      const fieldFormData = {
+        type: this.field.type,
+        label: this.field.label,
+        machineName: this.field._meta.camel,
+        description: this.field.description,
+        required: this.field.required,
+        many: this.field.many,
+        references: this.field.references
+      }
+
+      const fieldFormDataOriginal = _.cloneDeep(fieldFormData)
+
       return {
         opened: false,
         valid: false,
         rules: {
           required: [
-            (v) => !!v || 'Required' // @todo replace with $t
+            (v) => !!v || this.$t('entities.fields.validations.required')
           ],
           references: [
-            (v) => (this.fieldFormData.type !== 'reference' || !!v) || 'Select an entity to reference' // @todo replace with $t
+            (v) => (this.fieldFormData.type !== 'reference' || !!v) || this.$t('entities.fields.validations.selectReference')
           ]
         },
         icon: this.isNew ? 'add' : 'edit',
         iconBackgroundColor: this.isNew ? 'primary' : 'amber darken-1',
-        fieldFormData: {
-          type: this.field.type,
-          label: this.field.label,
-          machineName: this.field._meta.camel,
-          description: this.field.description,
-          required: this.field.required,
-          many: this.field.many,
-          references: this.field.references
-        },
+        fieldFormData,
+        fieldFormDataOriginal,
         // @todo get field types via API call rather than statically defined.
         fieldTypes: [
-          {value: 'id', text: this.$t('entities.fields.types.id')},
           {value: 'int', text: this.$t('entities.fields.types.int')},
           {value: 'string', text: this.$t('entities.fields.types.string')},
           {value: 'reference', text: this.$t('entities.fields.types.reference')},
@@ -79,11 +108,38 @@
         ],
         // @todo get entities from store once EntityList component stores its state in vuex.
         entityTypes: ['Dog', 'Cat'],
-        machineNameManuallyAltered: false
+        machineNameAlterable: this.isNew
       }
     },
     methods: {
-      save() {
+      disableFormElement(element) {
+        let disable = false
+
+        // Disable rules.
+        if (this.fieldFormData.type === 'id' && element === 'machineName') {
+          disable = true
+        }
+
+        return disable
+      },
+      showFormElement(element) {
+        let show = true
+
+        // Hide rules.
+        if ( this.fieldFormData.type !== 'reference' && element === 'references'
+          || this.fieldFormData.type === 'id' && element === 'type'
+          || this.fieldFormData.type === 'id' && element === 'required'
+          || this.fieldFormData.type === 'id' && element === 'many'
+          || this.fieldFormData.type === 'id' && element === 'machineNameLockText'
+          || this.fieldFormData.type === 'id' && element === 'machineNameAutoText'
+          || this.fieldFormData.type !== 'id' && element === 'machineNameLockIdText'
+        ) {
+          show = false
+        }
+
+        return show
+      },
+      submit() {
 
         if (!this.$refs.fieldEditForm.validate()) {
           return
@@ -106,7 +162,7 @@
             description: this.fieldFormData.description,
             many: this.fieldFormData.many,
             required: this.fieldFormData.required,
-            references: this.fieldFormData.references,
+            references: this.fieldFormData.type === 'reference' ? this.fieldFormData.references : null,
           }
         })
 
@@ -118,22 +174,27 @@
           this.$refs.fieldEditForm.reset()
         }
 
-        this.machineNameManuallyAltered = false
+        // Saved data should now be considered the original data.
+        this.fieldFormDataOriginal = _.cloneDeep(this.fieldFormData)
+
+        this.machineNameAlterable = false
         this.opened = false
       },
-      autoPopulateMachineName() {
-        // Prevent altering machine names when editing fields.
-        if (!this.isNew) {
-          return
-        }
-        // Allow auto population if machine name is empty, even if has been manually altered (likely deleted).
-        if (!this.fieldFormData.machineName) {
-          this.machineNameManuallyAltered = false
-        }
+      cancel() {
+        this.fieldFormData = _.cloneDeep(this.fieldFormDataOriginal)
+        this.opened = false
+      },
+      deriveMachineNameFromLabel() {
         // Only proceed with auto population if the machine name hasn't been manually altered.
-        if (!this.machineNameManuallyAltered) {
+        if (this.fieldFormData.machineName === '') {
+          this.machineNameAlterable = true
+        }
+        if (this.machineNameAlterable) {
           this.fieldFormData.machineName = _(this.fieldFormData.label).camelCase()
         }
+      },
+      machineNameChanged() {
+        this.fieldFormData.machineName === '' ? this.deriveMachineNameFromLabel() : (this.machineNameAlterable = false)
       }
     },
     props: {
