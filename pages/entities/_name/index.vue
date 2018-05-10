2058df8171e5e5cb3832fb55fc56ef2d1db07e7e
Barry Fisher <barry.fisher@reallifedigital.com>
2018-05-10 13:28:29 +0100

    Improves EntityTabs funcitonality

--- pages/entities/_name/index.vue
+++ pages/entities/_name/index.vue
@@ -11,39 +11,49 @@
           class="EntityTabs"
         >
 
+        <!-- Fixed tabs -->
         <v-tab
-          v-for="tab in tabs"
+          v-for="tab in fixedTabs"
           :key="tab.id"
           :href="`#tab-${tab.id}`"
+          class="EntityTabs__tab"
         >
+          <v-icon class="pr-2">{{ tab.icon }}</v-icon>
           {{ tab.label }}
         </v-tab>
 
+        <!-- Entity tabs -->
         <v-tab
           v-for="entity in openEntities"
           :key="entity.id"
           :href="`#tab-entity-${entity.id}`"
+          class="EntityTabs__tab"
         >
-          {{ entity.title }}
-
-          <v-icon small class="EntityTabs__closer" @click="closeTab(entity.id)">clear</v-icon>
+          <v-icon class="pr-2">web_asset</v-icon>
+          <span class="EntityTabs__tab-text">{{ entity.title }}</span>
+          <v-icon class="EntityTabs__closer" @click="closeTab(entity.id, $event)">clear</v-icon>
         </v-tab>
 
         <v-tabs-items>
-          <v-tab-item :key="`tab-list`" id="tab-list">
+
+          <!-- Fixed tabs content -->
+          <v-tab-item :key="`tab-list`" id="tab-list" class="EntityTabs__content">
             A list of entities
           </v-tab-item>
-          <v-tab-item :key="`tab-config`" id="tab-config">
+          <v-tab-item :key="`tab-config`" id="tab-config" class="EntityTabs__content">
             <EntityFields :entityType="entityType" :graphqlEndpoint="graphqlEndpoint" />
           </v-tab-item>
+
+          <!-- Entity tabs content -->
           <v-tab-item
             v-for="entity in openEntities"
             :key="`tab-${entity.id}`"
-            :id="`tab-entity-${entity.id}`">
-            Id: {{ entity.id }}
-            <br />
-            Title: {{ entity.title }}
+            :id="`tab-entity-${entity.id}`"
+            class="EntityTabs__content"
+          >
+            Content: {{ entity.content }}
           </v-tab-item>
+
         </v-tabs-items>
 
         </v-tabs>
@@ -56,6 +66,7 @@
 <script>
 import EntityList from '@/components/EntityList.vue'
 import EntityFields from '@/components/EntityFields.vue'
+import _ from 'lodash'
 
 export default {
   components: {
@@ -68,20 +79,57 @@ export default {
     }
   },
   methods: {
-    closeTab(id) {
-      const closingTabIndex = this.openEntities.findIndex(entity => entity.id === id)
-      const closingTabIsActive = 'tab-entity-' + this.openEntities[closingTabIndex].id === this.activeTab
-      this.openEntities.splice(closingTabIndex, 1)
+    closeTab(id, event) {
 
-      // @todo fix this
-      if (closingTabIsActive) {
-        if (this.openEntities.length > 0) {
-          this.activeTab = 'tab-entity-' + this.openEntities[closingTabIndex].id
-        }
-        else {
-          this.activeTab = 'tab-' + this.tabs[this.tabs.length-1].id
-        }
-      }
+      const originatingTab = this.activeTab
+
+      const closingTabIndex = this.openEntities.findIndex(entity => entity.id === id)
+      const isFinalTabClosing = closingTabIndex === this.openEntities.length -1 ? true : false
+      const closingTabIsActive = 'tab-entity-' + this.openEntities[closingTabIndex].id === this.activeTab
+
+      this.$root.$nextTick(() => {
+
+        // Wait a second to allow tab content to pan into
+        // view before asking confimation to close.
+        setTimeout(() => {
+
+          // @todo replace with v-dialog confirmation
+          const action = confirm("Are you sure you want to close this tab?")
+          if (!action) {
+            this.activeTab = originatingTab
+            return
+          }
+
+          event.target.parentElement.parentElement.classList.add('EntityTabs__tab--closing')
+
+          setTimeout(() => {
+
+            this.openEntities.splice(closingTabIndex, 1)
+
+            if (closingTabIsActive) {
+
+              if (this.openEntities.length > 0) {
+                // Set active tab to nearest open entity.
+                const newFinalTab = this.openEntities[this.openEntities.length-1]
+                const samePositionTab = this.openEntities[closingTabIndex]
+                const newActiveTab = isFinalTabClosing ? newFinalTab : samePositionTab
+                this.activeTab = 'tab-entity-' + newActiveTab.id
+              }
+              else {
+                // Set active tab to last fixed tab.
+                this.activeTab = 'tab-' + this.fixedTabs[this.fixedTabs.length-1].id
+              }
+
+            }
+            else {
+              this.activeTab = originatingTab
+            }
+
+          }, 200)
+
+        }, closingTabIsActive ? 0 : 1000)
+
+      })
 
     }
   },
@@ -91,15 +139,21 @@ export default {
     return {
       // @todo link graphql error with store.
       graphqlError: false,
-      tabs: [
+      fixedTabs: [
         // @todo add translate string
-        {id: 'list', label: 'List'},
-        {id: 'config', label: 'Config'},
+        {id: 'list', label: 'List', icon: 'list'},
+        {id: 'config', label: 'Config', icon: 'settings'},
       ],
       activeTab: 'tab-list',
       openEntities: [
-        {id: '123456789', title: 'Beautiful Case Study', content: 'Test 123'},
-        {id: '24681012', title: 'Large Case Study', content: 'Test 456'},
+        // {id: '1', title: 'Beautiful Case Study', content: 'Test 123'},
+        // {id: '2', title: 'Large Case Study', content: 'Test 456'},
+        // {id: '3', title: 'Covent Garden Exhibition 2018', content: 'Test 789'},
+        // {id: '4', title: 'Brunch on Tuesday', content: 'Test 789'},
+        // {id: '5', title: 'Another case study', content: 'Test 789'},
+        // {id: '6', title: 'This is a very long title to see what happens when a long title is set', content: 'Test 789'},
+        // {id: '7', title: 'Yet another case study', content: 'Test 789'},
+        // {id: '8', title: 'And another for good luck', content: 'Test 789'},
       ]
     }
   },
@@ -113,10 +167,33 @@ export default {
 </script>
 
 <style lang="scss">
-.EntityTabs__closer {
-  padding: 0 0.5rem;
-  &:hover {
-    color: red;
+
+.EntityTabs {
+
+  &__closer {
+    padding: 0 0.5rem;
+    &:hover {
+      color: red;
+    }
   }
+
+  &__tab {
+    transition: all 0.2s;
+    overflow-x: hidden;
+
+    &--closing {
+      transform: translateX(-100%);
+      opacity: 0;
+    }
+  }
+
+  &__tab-text {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 200px;
+  }
+
 }
+
 </style>
