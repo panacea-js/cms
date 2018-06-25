e81f2949c6a93e3c76a32a106bb86c3136e878f9
Barry Fisher <barry.fisher@reallifedigital.com>
2018-06-25 03:54:07 +0100

    Refactor to sortable object fields

--- components/EntityEditFields.vue
+++ components/EntityEditFields.vue
@@ -29,6 +29,7 @@
                 <v-flex xs9 offset-xs1>
                   <component
                     :is="getFieldComponent(field.type)"
+                    :key="`${field.key}.${index}--input`"
                     :prefix="`${index+1}.`"
                     :value="values[field._meta.camel][index]"
                     v-model="values[field._meta.camel][index]"
@@ -72,9 +73,21 @@
 
           <!-- Many nested object values -->
           <div v-if="Array.isArray(values[field._meta.camel])" class="EntityEditFields__sortable-group">
+            <v-layout>
+              <v-flex xs12 ml-3>
+                <v-switch v-model="reordering" :key="`${field.key}--reorder`" label="Reorder" v-if="values[field._meta.camel].length > 1"></v-switch>
+              </v-flex>
+            </v-layout>
             <v-layout class="EntityEditFields__value-object-item EntityEditFields__value-object-item--many EntityEditFields__sortable-item" v-for="(item, delta) in values[field._meta.camel]" :key="`${field._meta.camel}.${delta}`">
-              <v-flex xs1 d-inline-flex justify-center>
-                <div class="EntityEditFields__value-object-item-delta-text">{{ delta + 1 }} <v-icon class="EntityEditFields__item-dragger">drag_handle</v-icon></div>
+              <v-flex xs1 text-xs-center pl-2>
+                <v-chip class="primary">
+                  <v-avatar v-if="reordering" class="EntityEditFields__item-dragger mr-0">
+                    <v-icon light>drag_handle</v-icon>
+                  </v-avatar>
+                  <span class="EntityEditFields__value-object-item-delta-text">
+                    {{ delta + 1 }}
+                  </span>
+                </v-chip>
               </v-flex>
               <v-flex xs10>
                 <v-card>
@@ -84,13 +97,13 @@
                 </v-card>
               </v-flex>
               <v-flex xs1 text-xs-center>
-                <v-icon class="EntityEditFields__remove-object-item" v-if="values[field._meta.camel].length > 1 || !field.required" @click="removeItem(field._meta.camel, values, delta)">clear</v-icon>
+                <v-icon class="EntityEditFields__remove-object-item" v-if="(values[field._meta.camel].length > 1 || !field.required) && !reordering" @click="removeItem(field._meta.camel, values, delta)">clear</v-icon>
               </v-flex>
             </v-layout>
           </div>
 
           <!-- Add another nested object -->
-          <v-layout row wrap v-if="field.many" class="EntityEditFields__add-object-item">
+          <v-layout row wrap v-if="field.many && !reordering" class="EntityEditFields__add-object-item">
             <v-flex xs12 text-xs-center>
               <v-btn fab small color="primary" @click="addAnotherNestedField(values, field)">
                 <v-icon color="grey darken-4">add</v-icon>
@@ -111,15 +124,17 @@ import Sortable from 'sortablejs'
 
 export default {
   name: 'EntityEditFields',
-  mounted() {
-    this.initialiseSortableItems()
+  data() {
+    return {
+      reordering: false,
+      sortableAdded: false
+    }
   },
   methods: {
     getFieldComponent(fieldType) {
        return 'v-text-field'
     },
     addAnotherNestedField(values, field) {
-
       const recurseFields = (field) => {
         const structure = {}
         _(field.fields).forEach(subField => {
@@ -157,8 +172,20 @@ export default {
       values[fieldId].splice(delta, 1)
       this.$forceUpdate()
     },
+    shrinkSortableItems() {
+      const fieldsToCollapse = this.$el.querySelectorAll(':scope .EntityEditFields__sortable-item .EntityEditFields__field:nth-child(n+2)')
+      fieldsToCollapse.forEach(field => field.classList.add('EntityEditFields__field--collapsed'))
+    },
+    unShrinkSortableItems() {
+      this.$el.querySelectorAll(':scope .EntityEditFields__field--collapsed').forEach(field => field.classList.remove('EntityEditFields__field--collapsed'))
+    },
     initialiseSortableItems () {
+      if (this.sortableAdded) {
+        return
+      }
+
       const sortableGroups = this.$el.querySelectorAll('.EntityEditFields__sortable-group')
+
       sortableGroups.forEach(sortableGroup => {
         new Sortable(
           sortableGroup,
@@ -167,18 +194,19 @@ export default {
             handle: '.EntityEditFields__item-dragger',
             scrollSensitivity: 0,
             scrollSpeed: 1,
-            onEnd: function (event) {
-              event.to.querySelectorAll(':scope .EntityEditFields__field--collapsed').forEach(field => field.classList.remove('EntityEditFields__field--collapsed'))
-            },
-            onStart: function (event) {
-              const fieldsToCollapse = event.from.querySelectorAll('.EntityEditFields__sortable-item .EntityEditFields__field:nth-child(n+2)')
-              fieldsToCollapse.forEach(field => field.classList.add('EntityEditFields__field--collapsed'))
-            },
             animation: 250
           }
         )
       })
+
+      this.sortableAdded = true
     },
+  },
+  watch: {
+    reordering(value) {
+      value ? this.shrinkSortableItems() : this.unShrinkSortableItems()
+      this.initialiseSortableItems()
+    }
   },
   props: {
     fields: {
@@ -196,7 +224,6 @@ export default {
 .EntityEditFields
   &__field
     &--collapsed
-      background hotpink
       max-height 0
       overflow hidden
   &__field-object, &__field-scalar-many
@@ -214,16 +241,7 @@ export default {
     :root .theme--light &
       border-color white
   &__value-object-item-delta-text
-    flex 0 0 auto !important
-    width 25px
-    height 25px
-    padding-top 3px
-    margin-left 1rem
-    border-radius 50px
-    text-align center
     font-weight bold
-    background: $color-primary
-    color: $grey.darken-3
   &__item-dragger
     cursor move
   &__add-object-item
@@ -237,9 +255,9 @@ export default {
       cursor pointer
       color: $red.darken-1 !important
 .sortable-ghost
-  background: $grey.base
+  background: $color-primary
 .sortable-chosen
-  opacity 0.1
+  opacity 0.2
   .EntityEditFields__field:nth-child(n+2)
     display: none
 </style>
