5378480f0f42c8c0e6b973b9495f4f3fad95e230
Barry Fisher <barry.fisher@reallifedigital.com>
2018-07-05 17:18:46 +0100

    Change PANACEA-CMS-LANGUAGE to PANACEA-LANGUAGE to match core expected cookie

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -29,7 +29,7 @@ export default {
 
           document.cookie.split('; ').map(cookie => {
             const [ key, value ] = cookie.split('=')
-            if (key === 'PANACEA-CMS-LANGUAGE') {
+            if (key === 'PANACEA-LANGUAGE') {
               activeLanguage = value
             }
           }).filter(x => !!x)[0]
@@ -40,7 +40,7 @@ export default {
       set: function (language) {
         if (typeof document !== 'undefined') {
           // Set cookie and refresh the page to re-instantiate i18n plugin.
-          document.cookie = `PANACEA-CMS-LANGUAGE=${language}; path=/`
+          document.cookie = `PANACEA-LANGUAGE=${language}; path=/`
           location = location
         }
       }
